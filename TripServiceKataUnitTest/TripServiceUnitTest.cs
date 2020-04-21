a1cc2e8070bc0c09d13b3c300dc739e43ec8b2f7
Mathieu RACASSIN <mathieu.racassin@lacombedulionvert.fr>
2020-04-21 22:08:06 +0200

    Ajoutez des fichiers projet.

---
+++ TripServiceKataUnitTest/TripServiceUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TripServiceKataUnitTest
+{
+    [TestClass]
+    public class TripServiceUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
